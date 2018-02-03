dfcdd8a309fcef8606e40d903e9281a8b9b2cdb5
Dendi 'Drizzer' Handian <dendihandian@gmail.com>
2018-02-04 00:17:34 +0700

    examples of importing function from a file

--- src/main.js
+++ src/main.js
@@ -1,1 +1,6 @@
-alert('main');
+import notification from './notification';
+import {greet} from './notification';
+
+notification.annouce('here we go as alert');
+notification.log('here we go');
+greet();

2c9845c884b25394ae11b33fcdf8ad2f0fe7d794
Dendi 'Drizzer' Handian <dendihandian@gmail.com>
2018-02-04 00:49:04 +0700

    example of loading css and the style in main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,1 @@
-import notification from './notification';
-import {greet} from './notification';
-
-notification.annouce('here we go as alert');
-notification.log('here we go');
-greet();
+require('./main.css');

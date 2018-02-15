fa873dabd74d3d023073119e8ed36b940450f3cf
Dendi 'Drizzer' Handian <dendihandian@gmail.com>
2018-02-15 18:30:51 +0700

    configure webpack for scss

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.s[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       },
